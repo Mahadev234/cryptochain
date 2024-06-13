98ae41582d19cf2e06fe99bbee007f69d1d22c2a
Mahadev234 <mstelidevara@gmail.com>
2024-06-13 10:14:29 +0530

    add chain replace functionality

--- blockChain.js
+++ blockChain.js
@@ -5,6 +5,7 @@ class BlockChain {
     constructor() {
         this.chain = [Block.genesis()]
     }
+
     addBlock({ data }) {
         const newBlock = Block.mineBlock({
             lastBlock: this.chain[this.chain.length - 1],
@@ -12,6 +13,7 @@ class BlockChain {
         })
         this.chain.push(newBlock)
     }
+
     static isValidChain(chain) {
         if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
             return false
@@ -26,6 +28,19 @@ class BlockChain {
             if (hash !== validateHash) return false
         }
         return true
+    }
+
+    replaceChain(chain) {
+        if (chain.length <= this.chain.length) {
+            console.log('the incoming chain must be longer')
+            return;
+        }
+        if (!BlockChain.isValidChain(chain)) {
+            console.log('the incoming chain must be valid')
+            return;
+        }
+        console.log('replacing the chain with ', chain)
+        this.chain = chain
     }
 }
 module.exports = BlockChain
--- blockchain.test.js
+++ blockchain.test.js
@@ -2,9 +2,12 @@ const BlockChain = require('./blockChain')
 const Block = require('./block')
 
 describe('Blockchain', () => {
-    let blockChain;
+    let blockChain, newChain, originalChain;
     beforeEach(() => {
         blockChain = new BlockChain()
+        newChain = new BlockChain()
+
+        originalChain = blockChain.chain;
     })
 
     it('contains the `chain` array instance', () => {
@@ -31,36 +34,77 @@ describe('Blockchain', () => {
                 expect(BlockChain.isValidChain(blockChain.chain)).toBe(false)
             })
         })
+
+        describe("when the chain starts with genesis block and has multiple blocks", () => {
+            beforeEach(() => {
+                blockChain.addBlock({ data: "data-1" })
+                blockChain.addBlock({ data: "data-2" })
+                blockChain.addBlock({ data: "data-3" })
+            })
+
+            describe("and a lastHash reference is changed", () => {
+                it("returns false", () => {
+                    blockChain.chain[2].lastHash = "modHash"
+
+                    expect(BlockChain.isValidChain(blockChain.chain)).toBe(false)
+                })
+            })
+
+            describe("and the chain contains a block with an invalid field", () => {
+                it("returns false", () => {
+                    blockChain.chain[2].data = "modData"
+
+                    expect(BlockChain.isValidChain(blockChain.chain)).toBe(false)
+                })
+            })
+
+            describe("and the chain doesn't contain any invalid blocks", () => {
+                it("returns true", () => {
+
+                    expect(BlockChain.isValidChain(blockChain.chain)).toBe(true)
+                })
+            })
+        })
     })
 
-    describe("when the chain starts with genesis block and has multiple blocks", () => {
+    describe("replaceChain()", () => {
+        let errorMock, logMock;
         beforeEach(() => {
-            blockChain.addBlock({ data: "data-1" })
-            blockChain.addBlock({ data: "data-2" })
-            blockChain.addBlock({ data: "data-3" })
+            errorMock = jest.fn();
+            logMock = jest.fn();
+
+            global.console.error = errorMock
+            global.console.log = logMock
         })
-
-        describe("and a lastHash reference is changed", () => {
-            it("returns false", () => {
-                blockChain.chain[2].lastHash = "modHash"
-
-                expect(BlockChain.isValidChain(blockChain.chain)).toBe(false)
+        describe("when the new chain is not longer", () => {
+            it("doesn't replace the chain", () => {
+                newChain.chain[0] = { new: "chain" }
+                blockChain.replaceChain(newChain.chain)
+                expect(blockChain.chain).toEqual(originalChain)
             })
         })
 
-        describe("and the chain contains a block with an invalid field", () => {
-            it("returns false", () => {
-                blockChain.chain[2].data = "modData"
-
-                expect(BlockChain.isValidChain(blockChain.chain)).toBe(false)
+        describe("when the new chain is longer", () => {
+            beforeEach(() => {
+                newChain.addBlock({ data: "data-1" })
+                newChain.addBlock({ data: "data-2" })
+                newChain.addBlock({ data: "data-3" })
             })
-        })
+            describe("and the chain is invalid", () => {
+                it("doesn't replace the chain", () => {
+                    newChain.chain[2].hash = "modHash"
+                    blockChain.replaceChain(newChain.chain)
+                    expect(blockChain.chain).toEqual(originalChain)
+                })
+            })
 
-        describe("and the chain doesn't contain any invalid blocks", () => {
-            it("returns true", () => {
-
-                expect(BlockChain.isValidChain(blockChain.chain)).toBe(true)
+            describe("and the chain is valid", () => {
+                it("replaces the chain", () => {
+                    blockChain.replaceChain(newChain.chain)
+                    expect(blockChain.chain).toEqual(newChain.chain)
+                })
             })
         })
     })
+
 }) 
